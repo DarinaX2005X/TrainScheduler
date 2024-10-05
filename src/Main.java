6f3dc5f0ffd4076c3d58ed8e2e12e09d2a1a08d8
uuuuuu-ops <230643@astanait.edu.kz>
2024-10-05 19:19:14 +0500

    новые классы как passenger station cargotrain maintenance station

--- src/Main.java
+++ src/Main.java
@@ -38,12 +38,19 @@ class Train {
     }
 }
 
+
 interface TrainOperations {
     void startEngine();
     void stopEngine();
 }
 
-class ElectricTrain extends Train implements TrainOperations {
+
+interface MaintenanceOperations {
+    void performMaintenance();
+}
+
+
+class ElectricTrain extends Train implements TrainOperations, MaintenanceOperations {
 
     public ElectricTrain(String trainId, String trainType, String departureTime) {
         super(trainId, trainType, departureTime);
@@ -58,9 +65,15 @@ class ElectricTrain extends Train implements TrainOperations {
     public void stopEngine() {
         System.out.println("Electric engine stopped.");
     }
+
+    @Override
+    public void performMaintenance() {
+        System.out.println("Performing maintenance on electric train.");
+    }
 }
 
-class DieselTrain extends Train implements TrainOperations {
+
+class DieselTrain extends Train implements TrainOperations, MaintenanceOperations {
 
     public DieselTrain(String trainId, String trainType, String departureTime) {
         super(trainId, trainType, departureTime);
@@ -75,7 +88,13 @@ class DieselTrain extends Train implements TrainOperations {
     public void stopEngine() {
         System.out.println("Diesel engine stopped.");
     }
+
+    @Override
+    public void performMaintenance() {
+        System.out.println("Performing maintenance on diesel train.");
+    }
 }
+
 
 class TrainController {
     private final TrainOperations train;
@@ -95,13 +114,90 @@ class TrainController {
     }
 }
 
+class Passenger {
+    private String name;
+    private String ticketNumber;
+    private String seatNumber;
+
+    public Passenger(String name, String ticketNumber, String seatNumber) {
+        this.name = name;
+        this.ticketNumber = ticketNumber;
+        this.seatNumber = seatNumber;
+    }
+
+    public void displayPassengerInfo() {
+        System.out.println("Passenger: " + name + ", Ticket: " + ticketNumber + ", Seat: " + seatNumber);
+    }
+}
+
+
+class Station {
+    private String stationName;
+    private String location;
+
+    public Station(String stationName, String location) {
+        this.stationName = stationName;
+        this.location = location;
+    }
+
+    public void displayStationInfo() {
+        System.out.println("Station: " + stationName + ", Location: " + location);
+    }
+}
+
+
+class CargoTrain extends Train implements TrainOperations {
+    private double cargoWeight;
+
+    public CargoTrain(String trainId, String trainType, String departureTime, double cargoWeight) {
+        super(trainId, trainType, departureTime);
+        this.cargoWeight = cargoWeight;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println("Cargo train engine started.");
+    }
+
+    @Override
+    public void stopEngine() {
+        System.out.println("Cargo train engine stopped.");
+    }
+
+    public void displayCargoInfo() {
+        System.out.println("Cargo weight: " + cargoWeight + " tons");
+    }
+}
+
+
+class Maintenance {
+    private String lastServiceDate;
+    private int serviceIntervalDays;
+
+    public Maintenance(String lastServiceDate, int serviceIntervalDays) {
+        this.lastServiceDate = lastServiceDate;
+        this.serviceIntervalDays = serviceIntervalDays;
+    }
+
+    public void displayMaintenanceInfo() {
+        System.out.println("Last service: " + lastServiceDate + ", Service interval: " + serviceIntervalDays + " days");
+    }
+
+    public boolean isServiceDue(String currentDate) {
+        System.out.println("Checking if service is due...");
+        return true;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         Train electricTrain = new ElectricTrain("E123", "Electric", "10:00");
         Train dieselTrain = new DieselTrain("D456", "Diesel", "12:00");
 
+
         TrainController electricController = new TrainController((TrainOperations) electricTrain);
         TrainController dieselController = new TrainController((TrainOperations) dieselTrain);
+
 
         electricController.startJourney();
         electricTrain.displayTrainInfo();
@@ -110,6 +206,23 @@ public class Main {
         dieselController.startJourney();
         dieselTrain.displayTrainInfo();
         dieselController.endJourney();
+
+
+        CargoTrain cargoTrain = new CargoTrain("C789", "Cargo", "14:00", 50);
+        cargoTrain.startEngine();
+        cargoTrain.displayCargoInfo();
+        cargoTrain.stopEngine();
+
+
+        Passenger passenger = new Passenger("John Doe", "T123", "12A");
+        passenger.displayPassengerInfo();
+
+        Station station = new Station("Central", "Astana");
+        station.displayStationInfo();
+
+
+        Maintenance maintenance = new Maintenance("2024-01-01", 180);
+        maintenance.displayMaintenanceInfo();
+        maintenance.isServiceDue("2024-06-01");
     }
 }
-
